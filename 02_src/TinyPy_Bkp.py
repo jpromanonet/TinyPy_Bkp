1bf7ea9800e8dab983d32b2721f910745491a8cf
Juan P. Romano <35672464+jpromanonet@users.noreply.github.com>
2019-02-22 16:40:48 -0300

    First iteration
    Still not working.

--- 02_src/TinyPy_Bkp.py
+++ 02_src/TinyPy_Bkp.py
@@ -1,1 +1,22 @@
 #! python3
+
+# TinyPy_Bkp is an incremental tiny and fast backup system write in python3
+
+# Importing libraries and frameworks
+
+import os
+import sys
+import shutil
+import zipfile
+
+# Defining global variables and scope
+
+folder_To_Zip = None
+folder = None
+files_Number = 0
+
+# Defining functions
+
+# Defining user parameters
+
+# Program logic
